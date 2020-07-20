12d79f715f5399d99264c9b6e84035b993fe20e0
GuoBin <guobin201314@gmail.com>
2020-07-20 11:47:24 +0800

    feat(c): 基本数据结构与语句

--- code_c/1_print/main.c
+++ code_c/1_print/main.c
@@ -1,7 +1,60 @@
 #include <stdio.h>
 
+int x, y;
+
+int add() {
+    x = 1;
+    y = 2;
+    return x + y;
+}
+
+int count;
+
+extern void write_extern();
+extern void m_build_array();
+extern void m_pointer_test();
+
+struct User {
+    int age;
+};
+
 int main() {
     int age = 20;
+    char one_bit = 'Z';
+    // 无符号
+    unsigned char un_one_bit = 'a';
+    short s_int = 32767;
+    long l_int = 2147;
     printf("age is %d\n", age);
+    printf("one_bit %d\n", one_bit);
+
+    int res = add();
+    printf("res is %d\n", res);
+
+    count = 5;
+    write_extern();
+
+    m_build_array();
+    // 结构体
+    struct User user;
+    user.age = 18;
+    printf("user age is %d\n", user.age);
+    // 指针
+    m_pointer_test();
+
+//    int c;
+//    printf("Enter a value: \n");
+//    c = getchar();
+//    printf("You entered: ");
+//    putchar(c);
+//    printf("\n");
+
+    char input_str[100];
+    printf("Enter String: \n");
+    scanf("%s", input_str);
+    printf("You Write\n");
+    puts(input_str);
+    printf("\n");
+
     return 0;
 }
---
+++ code_c/1_print/support.c
@@ -0,0 +1,35 @@
+//
+// Created by 郭斌 on 2020/7/20.
+//
+#include <stdlib.h>
+#include <stdio.h>
+
+extern int count;
+
+void write_extern(void) {
+    printf("count is %d\n", count);
+}
+
+void m_build_array() {
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        arr[i] = i;
+    }
+    printf("arr is %n\n", arr);
+    printf("arr[0] is %d\n", arr[0]);
+    printf("size is %lu\n", sizeof(arr));
+}
+
+void m_pointer_test() {
+    int price;
+    char str[10] = "abcdefg123";
+    // &price = 0x7ffee2ddf6a8
+    printf("price 变量的地址： %p\n", &price);
+    printf("str 变量的地址： %p\n", &str);
+    // 创建指正变量
+    char *p = malloc(10);
+    *p = 'a';
+    *(p + 1) = 'b';
+    printf("%s\n", p);
+    free(p);
+}
